129f054e90d482eba6a30f2cd4b7e1c735870d4a
Markus Vikholm <markuvik@ety-a2123b-13.edu.wks.metropolia.fi>
2018-02-01 10:26:25 +0200

    jou

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,26 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { RoutesComponent } from './routes/routes.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
+import { SetupComponent } from './setup/setup.component';
+import {HttpClientModule} from '@angular/common/http';
+import {DigitransitService} from './services/digitransit.service';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RoutesComponent,
+    TopBarComponent,
+    SetupComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+      HttpClientModule
   ],
-  providers: [],
+  providers: [DigitransitService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
